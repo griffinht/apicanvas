4290b2eab48ccc15bff063ecbc5330f2ebcc3244
Griffin Tomaszewski <code@griffinht.com>
2025-02-01 19:32:33 -0500

    impl response delete node

---
+++ src/openapi/paths/methods/response/Delete.tsx
@@ -0,0 +1,13 @@
+import { ReactFlowInstance } from '@xyflow/react';
+import { getLayoutedElements } from '../../../../Layout';
+
+export function deleteResponseNode(nodeId: string, rfInstance: ReactFlowInstance, direction: 'TB' | 'LR') {
+  const nodes = rfInstance.getNodes().filter(node => node.id !== nodeId);
+  const edges = rfInstance.getEdges().filter(edge => 
+    edge.source !== nodeId && edge.target !== nodeId
+  );
+
+  const { nodes: layoutedNodes, edges: layoutedEdges } = getLayoutedElements(nodes, edges, direction);
+  rfInstance.setNodes(layoutedNodes);
+  rfInstance.setEdges(layoutedEdges);
+}
