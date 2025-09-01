78e24235cba4ad7b21ca08e74a27887a40d9e998
Griffin Tomaszewski <55036602+griffinht@users.noreply.github.com>
2025-09-01 13:46:52 -0400

    Update Version.tsx

--- src/dragbar/Version.tsx
+++ src/dragbar/Version.tsx
@@ -12,7 +12,7 @@ export function Version() {
       marginTop: '4px'
     }}>
       <a 
-        href={`https://example.com`}
+        href={`https://github.com/griffinht/oas2tree2/releases/tag/v${version}`}
         style={{
           color: 'inherit',
           textDecoration: 'none'
